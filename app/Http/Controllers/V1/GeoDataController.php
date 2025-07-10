dd8acd457688b0fa71d2978a3e3ca59738b74de7
Eduardo da Silva <eduardo.silva@devsquad.com>
2025-07-09 22:20:26 -0300

    feature/GPP-1 - Create BrasilAPI and IBGE services skeleton

--- app/Http/Controllers/V1/GeoDataController.php
+++ app/Http/Controllers/V1/GeoDataController.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\V1\GeoData\IndexRequest;
 use Illuminate\Http\Request;
 
 class GeoDataController extends Controller
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 use App\Http\Controllers\V1\GeoDataController;
 use Illuminate\Support\Facades\Route;
 
