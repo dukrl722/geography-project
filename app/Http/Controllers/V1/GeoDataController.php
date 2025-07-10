5d185b864daab0db3a164a86de62d09bc5e6223e
Eduardo da Silva <eduardo.silva@devsquad.com>
2025-07-09 21:57:09 -0300

    feature/GPP-1 - Create controller skeleton

---
+++ app/Http/Controllers/V1/GeoDataController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\GeoData\IndexRequest;
+use Illuminate\Http\Request;
+
+class GeoDataController extends Controller
+{
+    public function index(string $code)
+    {
+
+    }
+}
