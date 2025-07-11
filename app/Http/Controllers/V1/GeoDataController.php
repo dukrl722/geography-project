c62dff5d791cbe59b6a91bb0f50844a725fc30f3
Eduardo da Silva <eduardo.silva@devsquad.com>
2025-07-10 22:13:51 -0300

    feature/GPP-1 - Create logic to change service called

--- app/Http/Controllers/V1/GeoDataController.php
+++ app/Http/Controllers/V1/GeoDataController.php
@@ -4,13 +4,37 @@ declare(strict_types = 1);
 
 namespace App\Http\Controllers\V1;
 
+use App\Actions\GeoData\GetStatesFromBrasilAPI;
+use App\Actions\GeoData\GetStatesFromIbgeAPI;
+use App\Enums\GeoDataProvider;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Resources\V1\StatesResource;
+use App\Services\BrasilAPI\Facades\BrasilAPI;
+use App\Services\IBGE\Facades\IBGE;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class GeoDataController extends Controller
 {
     public function index(string $code)
     {
+        $data = $this->getSelectedProviderData($code);
 
+        if (empty($data)) {
+            return response()->json([
+                'message' => 'No data found for the provided code.',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return StatesResource::collection($data);
+    }
+
+    private function getSelectedProviderData(string $code): array
+    {
+        if (config('services.geo_data.selected') === GeoDataProvider::BRASIL_API->value) {
+            return GetStatesFromBrasilAPI::execute($code);
+        }
+
+        return GetStatesFromIbgeAPI::execute($code);
     }
 }
