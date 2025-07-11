97220e5458f075e1669237743b42c0e43ff949ab
Eduardo da Silva <eduardo.silva@devsquad.com>
2025-07-10 21:06:08 -0300

    feature/GPP-1 - Create methods to ibge api cities endpoint

--- app/Services/IBGE/Endpoints/Cities.php
+++ app/Services/IBGE/Endpoints/Cities.php
@@ -4,7 +4,43 @@ declare(strict_types = 1);
 
 namespace App\Services\IBGE\Endpoints;
 
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
+
 class Cities
 {
+    protected string $url = 'localidades/estados';
 
+    protected Response $response;
+
+    protected PendingRequest $api;
+
+    public function __construct()
+    {
+        $this->api = Http::baseUrl(config('services.geo_data.ibge.base_url'));
+    }
+
+    public function search(string $code): static
+    {
+        $this->response = $this->api->get("$this->url/$code/municipios");
+
+        return $this;
+    }
+
+    public function json(): null | array
+    {
+        return $this->response->json();
+    }
+
+    public function collection(): Collection
+    {
+        return $this->response->collect();
+    }
+
+    public function response(): Response
+    {
+        return $this->response;
+    }
 }
