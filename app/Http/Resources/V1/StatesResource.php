7deee33f8a8f000ceaa47dcf47f814b375398279
Eduardo da Silva <eduardo.silva@devsquad.com>
2025-07-13 19:56:49 -0300

    feature/GPP-1 - Fix uppercase

--- app/Http/Resources/V1/StatesResource.php
+++ app/Http/Resources/V1/StatesResource.php
@@ -5,13 +5,14 @@ namespace App\Http\Resources\V1;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class StatesResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
         return [
-            'name' => strtoupper(Arr::get($this, 'nome')),
+            'name' => Str::upper(Arr::get($this, 'nome')),
             'ibge_code' => Arr::get($this, 'codigo_ibge') ?? Arr::get($this, 'id'),
         ];
     }
